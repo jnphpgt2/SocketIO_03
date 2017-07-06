55f4312e173c8a153854f50eadba3bf0ed20aead
jnphpgt2 <648793111@qq.com>
2017-07-06 17:11:45 +0800

    Initial commit

---
+++ src/bhz/utils/TestTimeJob.java
@@ -0,0 +1,26 @@
+package bhz.utils;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+class Temp extends Thread
+{
+    public void run()
+    {
+        System.out.println("run");
+    }
+}
+
+public class TestTimeJob
+{
+    public static void main(String args[]) throws Exception
+    {
+        Temp command = new Temp();
+        
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        ScheduledFuture<?> scheduleTask = scheduler.scheduleWithFixedDelay(command, 2, 3, TimeUnit.SECONDS);
+    }
+}
